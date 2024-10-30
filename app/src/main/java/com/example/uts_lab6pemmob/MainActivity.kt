2d66fd43498445092c19979d82ef088615524017
Lenovo <rizkiamalina1984@gmail.com>
2024-10-30 17:24:01 +0700

    Commit 1

---
+++ app/src/main/java/com/example/uts_lab6pemmob/MainActivity.kt
@@ -0,0 +1,13 @@
+// MainActivity.kt
+package com.example.greetingcardmaker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.uts_lab6pemmob.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/uts_lab6pemmob/MainActivity2.kt
@@ -0,0 +1,13 @@
+// MainActivity2.kt
+package com.example.greetingcardmaker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.uts_lab6pemmob.R
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2) // Make sure this points to activity_main2.xml
+    }
+}
