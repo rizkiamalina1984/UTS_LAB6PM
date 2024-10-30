01256d1a6066aa8ad430857d5a0b169b847ab3e7
Lenovo <rizkiamalina1984@gmail.com>
2024-10-31 01:32:14 +0700

    Commit 3

---
+++ app/src/main/java/com/example/uts_lab6pemmob/MainActivity5.kt
@@ -0,0 +1,13 @@
+package com.example.greetingcardmaker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.uts_lab6pemmob.R
+
+class MainActivity5 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main5)  // Set the updated layout file
+    }
+}
